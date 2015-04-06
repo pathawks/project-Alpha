3bc372f3da6997fcfcc04e82c15a0258bd49942f
Pat Hawks <pat@pathawks.com>
2015-04-05 18:11:36 -0700

    Reaching the end of the file is the more important condition

--- tifileutils/tifileutils.c
+++ tifileutils/tifileutils.c
@@ -110,13 +110,13 @@ int tiCalcToText(FILE* in, FILE* out) {
         	}
             switch( c ) {
             case 0xE1: // Goto token;            zero-terminated string follows
-                while (!(d = getc( in )) && --inFileSize);
+                while (--inFileSize && !(d = getc( in )));
                 ++inFileSize,ungetc( d, in );
             case 0xE0: // Lbl token;             zero-terminated string follows
             case 0x2D: // opening quote;         zero-terminated string follows
 				fprintf( out, "%s", TIFILEUTILS_TOKENS_85[c] );
             case 0x44: // literal-number token;  zero-terminated string follows
-				while ((c=getc( in )) && --inFileSize) {
+				while (--inFileSize && (c=getc( in ))) {
                     fprintf( out, "%c", c );
                 }
                 continue;
